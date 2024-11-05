b899ea9341881e5f42053733997faa371be82497
Doyeon <ansehdus7@gmail.com>
2024-11-05 17:11:43 +0900

    test : BoardService 테스트 작성

--- src/test/java/com/toyproject/project/domain/board/service/BoardServiceTest.java
+++ src/test/java/com/toyproject/project/domain/board/service/BoardServiceTest.java
@@ -2,12 +2,13 @@ package com.toyproject.project.domain.board.service;
 
 import com.toyproject.project.domain.board.domain.Board;
 import com.toyproject.project.domain.board.dto.BoardCreateRequestDto;
+import com.toyproject.project.domain.board.dto.BoardDetailResponseDto;
 import com.toyproject.project.domain.board.repository.BoardRepository;
 import com.toyproject.project.domain.matching.domain.Matching;
+import com.toyproject.project.domain.matching.dto.MatchingResponseDto;
 import com.toyproject.project.domain.matching.repository.MatchingRepository;
 import com.toyproject.project.domain.member.entity.Member;
 import com.toyproject.project.global.exception.CustomException;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,9 +18,13 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
+import static com.toyproject.project.domain.matching.domain.status.MatchingStatus.APPROVED;
+import static com.toyproject.project.domain.matching.domain.status.MatchingStatus.PENDING;
 import static com.toyproject.project.global.exception.ErrorCode.NO_AUTHORITY;
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -105,8 +110,8 @@ class BoardServiceTest {
         boardService.removeBoard(boardId, savedMember1);
 
         // then
-        verify(matchingRepository).deleteByBoardId(boardId); // 매칭 참가자 삭제 검증
-        verify(boardRepository).delete(savedBoard); // 게시글 삭제 검증
+        verify(matchingRepository).deleteByBoardId(boardId);
+        verify(boardRepository).delete(savedBoard);
     }
 
     @Test
@@ -118,7 +123,7 @@ class BoardServiceTest {
         when(boardRepository.findById(boardId)).thenReturn(Optional.of(savedBoard));
 
         // when
-        Assertions.assertThatThrownBy(() -> boardService.removeBoard(boardId, savedMember2))
+        assertThatThrownBy(() -> boardService.removeBoard(boardId, savedMember2))
                 .isInstanceOf(CustomException.class)
                 .hasFieldOrPropertyWithValue("ErrorCode", NO_AUTHORITY);
 
@@ -126,5 +131,80 @@ class BoardServiceTest {
         verify(matchingRepository, never()).deleteByBoardId(anyLong());
         verify(boardRepository, never()).delete(any(Board.class));
     }
+
+    @Test
+    @DisplayName("모집글 상세 조회 성공")
+    void getDetailBoards() {
+        // given
+        Long boardId = 1L;
+
+        when(boardRepository.findById(boardId)).thenReturn(Optional.of(savedBoard));
+
+        // when
+        BoardDetailResponseDto response = boardService.getDetailBoards(boardId);
+
+        // then
+        verify(boardRepository).findById(boardId);
+        assertThat(response.getId()).isEqualTo(savedBoard.getId());
+        assertThat(response.getTitle()).isEqualTo(savedBoard.getTitle());
+        assertThat(response.getDescription()).isEqualTo(savedBoard.getDescription());
+        assertThat(response.getMemberNickname()).isEqualTo(savedBoard.getMember().getNickname());
+        assertThat(response.getMaxParticipant()).isEqualTo(savedBoard.getMaxParticipant());
+        assertThat(response.getCurrentParticipant()).isEqualTo(savedBoard.getCurrentParticipant());
+        assertThat(response.getStartDate()).isEqualTo(savedBoard.getStartDate());
+        assertThat(response.getEndDate()).isEqualTo(savedBoard.getEndDate());
+    }
+
+    @Test
+    @DisplayName("동행 신청자 조회 성공")
+    void getMatchingList() {
+        // given
+        Long boardId = 1L;
+
+        when(boardRepository.findById(boardId)).thenReturn(Optional.of(savedBoard));
+        when(matchingRepository.findByBoardIdAndStatus(boardId, PENDING)).thenReturn(List.of(savedMatching));
+
+        // when
+        List<MatchingResponseDto> matchingList = boardService.getMatchingList(boardId, savedMember1);
+
+        // then
+        verify(boardRepository).findById(boardId);
+
+        assertThat(matchingList.size()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("동행 신청자 조회 실패: 권한 없음")
+    void getMatchingListFail() {
+        // given
+        Long boardId = 1L;
+
+        when(boardRepository.findById(boardId)).thenReturn(Optional.of(savedBoard));
+
+        // when
+        assertThatThrownBy(() -> boardService.getMatchingList(boardId, savedMember2))
+                .isInstanceOf(CustomException.class)
+                .hasFieldOrPropertyWithValue("ErrorCode", NO_AUTHORITY);
+
+        // then
+        verify(matchingRepository, never()).findByBoardIdAndStatus(anyLong(), any());
+    }
+
+    @Test
+    @DisplayName("현재 참가자 조회")
+    void getCurrentParticipant() {
+        // given
+        Long boardId = 1L;
+
+        when(matchingRepository.findByBoardIdAndStatus(boardId, APPROVED)).thenReturn(List.of(savedMatching));
+
+        // when
+        List<MatchingResponseDto> currentParticipant = boardService.getCurrentParticipant(boardId);
+
+        // then
+        verify(matchingRepository).findByBoardIdAndStatus(boardId, APPROVED);
+        assertThat(currentParticipant.size()).isEqualTo(1);
+    }
+
 
 }
