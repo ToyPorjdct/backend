3dedb8df5593ec7e62f8a6b7604d71c00c72a347
Doyeon <ansehdus7@gmail.com>
2024-11-26 16:15:11 +0900

    fix : 채팅방엔티티 회원목록 추가

--- src/main/java/com/toyproject/project/domain/chat/domain/ChatRoom.java
+++ src/main/java/com/toyproject/project/domain/chat/domain/ChatRoom.java
@@ -8,6 +8,8 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 
 @Getter
@@ -21,6 +23,9 @@ public class ChatRoom {
     @Column(name = "chat_room_id")
     private String id;
     private String name;
+
+    @Builder.Default
+    private Set<Long> memberlist = new HashSet<>();
 
     @CreatedDate
     private LocalDateTime createdAt;
