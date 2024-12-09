ff39944626608e13b1b9e4afea7b972eab4e9b35
Doyeon <ansehdus7@gmail.com>
2024-12-09 13:04:35 +0900

    fix: 어노테이션 추가

--- src/main/java/com/toyproject/project/domain/chat/dto/ChatMessage.java
+++ src/main/java/com/toyproject/project/domain/chat/dto/ChatMessage.java
@@ -1,10 +1,12 @@
 package com.toyproject.project.domain.chat.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class ChatMessage {
     private String message;
 }
--- src/main/java/com/toyproject/project/domain/chat/dto/ChatRoomRequest.java
+++ src/main/java/com/toyproject/project/domain/chat/dto/ChatRoomRequest.java
@@ -1,11 +1,13 @@
 package com.toyproject.project.domain.chat.dto;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class ChatRoomRequest {
     private String name;
     private Long otherMemberId;
