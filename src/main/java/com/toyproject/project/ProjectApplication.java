5fbda8a658c382eeeaa2fc2ff0d3d3ee5e2fd94f
Doyeon <ansehdus7@gmail.com>
2024-12-17 14:50:22 +0900

    fix: AuditingConfig 분리

--- src/main/java/com/toyproject/project/ProjectApplication.java
+++ src/main/java/com/toyproject/project/ProjectApplication.java
@@ -2,13 +2,9 @@ package com.toyproject.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableJpaAuditing
-@EnableMongoAuditing
 @EnableMongoRepositories(basePackages = "com.toyproject.project.domain.chat.repository.mongo")
 public class ProjectApplication {
 	public static void main(String[] args) {
